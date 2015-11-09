b3757748d4626e3da6d53f2b3f1383fc041f1b3e
Michael Cavalea <callmecavs@gmail.com>
2015-11-09 14:31:49 -0500

    add prototype progress function

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,12 @@ class Jax {
   constructor(url = '') {
     this.req = new XMLHttpRequest()
 
-    // this.req.addEventListener('progress', event => {
-    //   event.lengthComputable && progress(event.loaded / event.total)
-    // })
+    this.req.addEventListener('progress', event => {
+      event.lengthComputable && this.progress(event.loaded / event.total)
+    })
 
     this.req.addEventListener('readystatechange', event => {
-      this.req.readyState === XMLHttpRequest.DONE && this.req.status === 200
+      this.req.readyState === 4 && this.req.status === 200
         ? this.cb(JSON.parse(this.req.responseText))
         : undefined
     })
@@ -15,13 +15,13 @@ class Jax {
     this.req.open('GET', url)
   }
 
+  progress(func) {
+    this.progress = func || undefined
+    return this
+  }
+
   then(func) {
     this.cb = func
     this.req.send()
   }
 }
-
-const res = new Jax('http://localhost:3000/test.json')
-  .then(res => {
-    console.log(res)
-  })
