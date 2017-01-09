a21dc8ae5fcceccce5b1429b4e542dfe2c0659d4
callmecavs <callmecavs@gmail.com>
2017-01-08 20:25:52 -0500

    fix linting errors

--- src/jax.js
+++ src/jax.js
@@ -1,16 +1,18 @@
-export default (url) => {
+const jax = url => {
   return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest()
+    const request = new window.XMLHttpRequest()
     request.addEventListener('readystatechange', change.bind(undefined, request, resolve, reject))
     request.open('GET', url)
     request.send()
   })
 
-  function change(request, resolve, reject) {
-    if(request.readyState === request.DONE) {
+  function change (request, resolve, reject) {
+    if (request.readyState === request.DONE) {
       request.status === 200
         ? resolve(request.responseText)
         : reject(request.status)
     }
   }
 }
+
+export default jax
