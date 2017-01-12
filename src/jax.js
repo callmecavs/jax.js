12f08af290de6ac151650217e8978cb30d166176
Michael Joseph Cavalea <mcavalea@mcavalea-macbookpro2.roam.corp.google.com>
2017-01-12 15:21:43 -0500

    attempt at preferring fetch

--- src/jax.js
+++ src/jax.js
@@ -1,18 +1,22 @@
-const jax = url => {
-  return new Promise((resolve, reject) => {
-    const request = new window.XMLHttpRequest()
-    request.addEventListener('readystatechange', change.bind(undefined, request, resolve, reject))
-    request.open('GET', url)
-    request.send()
-  })
+let jax
 
-  function change (request, resolve, reject) {
+if('fetch' in window) {
+  jax = window.fetch
+} else {
+  const change = (request, resolve, reject) => {
     if (request.readyState === request.DONE) {
       request.status === 200
         ? resolve(request.responseText)
         : reject(request.status)
     }
   }
+
+  jax = url => new Promise((resolve, reject) => {
+    const request = new window.XMLHttpRequest()
+    request.addEventListener('readystatechange', () => change(request, resolve, reject))
+    request.open('GET', url)
+    request.send()
+  })
 }
 
 export default jax
